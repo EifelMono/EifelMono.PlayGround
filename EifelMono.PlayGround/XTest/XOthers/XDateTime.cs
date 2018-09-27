e156953145dfd20ecd96b0853e791086fe7e47c7
Andreas Klapperich <Andreas@Klapperich.de>
2018-09-27 11:19:58 +0200

    Play with out vars ....

---
+++ EifelMono.PlayGround/XTest/XOthers/XDateTime.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EifelMono.PlayGround.XCore;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EifelMono.PlayGround.XTest.XOthers
+{
+    public class XDateTime : XPlayGround
+    {
+        public XDateTime(ITestOutputHelper output) : base(output) { }
+
+        [Fact]
+        public void Test1()
+        {
+            var dateTime = DateTime.MinValue;
+
+            if (!DateTime.TryParse("xxx", out dateTime))
+                Output.WriteLine("Wrong DateTime");
+            else
+                Output.WriteLine("DateTime Ok");
+            Output.WriteLine($"DateTime={dateTime}");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            if (!DateTime.TryParse("xxx", out var dateTime))
+                Output.WriteLine("Wrong DateTime");
+            else
+                Output.WriteLine("DateTime Ok");
+            Output.WriteLine($"DateTime={dateTime}");
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            if (!DateTime.TryParse("2019.01.01", out var dateTime))
+                Output.WriteLine("Wrong DateTime");
+            else
+                Output.WriteLine("DateTime Ok");
+            Output.WriteLine($"DateTime={dateTime}");
+        }
+    }
+}
